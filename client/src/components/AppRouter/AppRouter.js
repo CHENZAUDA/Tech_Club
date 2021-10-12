e039b269d4ac172198c83e609fb6b39bcec6b87c
matan <you@example.com>
2021-10-12 14:43:09 +0300

    add social & blog component

--- client/src/components/AppRouter/AppRouter.js
+++ client/src/components/AppRouter/AppRouter.js
@@ -4,60 +4,71 @@ import Home from '../pages/Home'
 import DisplayForums from '../forum/DisplayForums'
 import ShowOneCategory from '../category/ShowOneCategory'
 import ShowAllCategories from '../category/ShowAllCategories'
-const AppRouter = () => {
-    return (
-        <>
-            <Router>
-                <Switch>
-                    <Route exact path="/">
-                        <Home/>
-                    </Route>
-                    <Route path="/auth/login">
-                        {/* {!user ? <Login/> : <Redirect to="/" />} */}
-                    </Route>
-                    <Route path="/auth/register">
-                        {/* {!user ? <Register/> : <Redirect to="/" />} */}
-                    </Route>
-                    <Route path="/category/create">
-                        {/* {!user ? <CreateCategory/> : <Redirect to="/auth/login" />} */}
-                    </Route>
-                    <Route path="/category/interviews">
-                        <ShowAllCategories/> 
-                    </Route>
-                    <Route path="/category">
-                        {/* {!user ? <DisplayCategories/> : <Redirect to="/auth/login" />} */}
-                    </Route>
-                    <Route path="/forum">
-                       <DisplayForums/> 
-                    </Route>
-                    
+import BlogRedirect from "../BlogRedirect/BlogRedirect";
+import BlogComponent from "../Blog/BlogComponent";
+import SocialMediaCom from '../Social Media/SocialMediaCom'
 
-                    <Route path="/forum/:id">
-                        {/* {!user ? <ShowOneForum/> : <Redirect to="/auth/login" />} */}
-                    </Route>
-                    <Route  path="/category/interviews/test">
-                        {/* <ShowOneCategory/> */}
-                        <Home/>
-                    </Route>
-                    <Route path="/forum/create/:id">
-                        {/* {!user ? <CreateForum/> : <Redirect to="/auth/login" />} */}
-                    </Route>
-                    <Route path="/thread/create/:id">
-                        {/* {!user ? <CreateThread/> : <Redirect to="/auth/login" />} */}
-                    </Route>
-                    <Route path="/thread/:id">
-                        {/* {!user ? <ShowOneThread/> : <Redirect to="/auth/login" />} */}
-                    </Route>
-                    <Route path="/thread">
-                        {/* {!user ? <DisplayThreads/> : <Redirect to="/auth/login" />} */}
-                    </Route>
-                    <Route path="/new">
-                        <ShowOneCategory/>
-                                            </Route>
-                </Switch>
-            </Router>
-        </>
-    )
-}
+const AppRouter = () => {
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/auth/login">
+            {/* {!user ? <Login/> : <Redirect to="/" />} */}
+          </Route>
+          <Route path="/auth/register">
+            {/* {!user ? <Register/> : <Redirect to="/" />} */}
+          </Route>
+          <Route path="/category/create">
+            {/* {!user ? <CreateCategory/> : <Redirect to="/auth/login" />} */}
+          </Route>
+          <Route path="/category/interviews">
+            <ShowAllCategories />
+          </Route>
+          <Route path="/category">
+            {/* {!user ? <DisplayCategories/> : <Redirect to="/auth/login" />} */}
+          </Route>
+          <Route path="/forum">
+            <DisplayForums />
+          </Route>
+          <Route path="/blogs">
+            <BlogComponent />
+          </Route>
+          <Route path="/blog">
+            <BlogRedirect />
+          </Route>
+          <Route path="/socialmedia">
+            <SocialMediaCom />
+          </Route>
+          <Route path="/forum/:id">
+            {/* {!user ? <ShowOneForum/> : <Redirect to="/auth/login" />} */}
+          </Route>
+          <Route path="/category/interviews/test">
+            {/* <ShowOneCategory/> */}
+            <Home />
+          </Route>
+          <Route path="/forum/create/:id">
+            {/* {!user ? <CreateForum/> : <Redirect to="/auth/login" />} */}
+          </Route>
+          <Route path="/thread/create/:id">
+            {/* {!user ? <CreateThread/> : <Redirect to="/auth/login" />} */}
+          </Route>
+          <Route path="/thread/:id">
+            {/* {!user ? <ShowOneThread/> : <Redirect to="/auth/login" />} */}
+          </Route>
+          <Route path="/thread">
+            {/* {!user ? <DisplayThreads/> : <Redirect to="/auth/login" />} */}
+          </Route>
+          <Route path="/new">
+            <ShowOneCategory />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
+};
 
 export default AppRouter
---
+++ client/src/components/BlogRedirect/BlogRedirect.jsx
@@ -0,0 +1,19 @@
+import React,{useState,useEffect} from 'react'
+import './blogredirect.css'
+
+export default function BlogRedirect() {
+    const [data , setData] = useState("")
+
+    // useEffect(() => {
+    //     fetch("http://localhost:8080/")
+    //     .then(res => res.json())
+    //     .then(res => console.log(res))
+    // }, [])
+
+    return (
+        <div className="redierct-container">
+            <button >צור פוסט</button>
+            <input className="comment-input" placeholder="...הוסף תגובה" type="text" />
+        </div>
+    )
+}
