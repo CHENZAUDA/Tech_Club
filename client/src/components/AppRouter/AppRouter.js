6cc330721cf7b8215083b6088798d0566fe65131
michal212 <michalfarada1997@gmail.com>
2021-10-13 00:34:51 +0300

    home-page

--- client/src/components/AppRouter/AppRouter.js
+++ client/src/components/AppRouter/AppRouter.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router, Switch ,Route} from 'react-router-dom'
-import Home from '../pages/Home'
+import Home from '../pages/Home/Home'
 import DisplayForums from '../forum/DisplayForums'
 import ShowOneCategory from '../category/ShowOneCategory'
 import ShowAllCategories from '../category/ShowAllCategories'
