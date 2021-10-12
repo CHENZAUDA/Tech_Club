0ee6d28d2307a67301b26c4183e2ea3b7ef37d74
CHEN ZAUDA <czauda@gmail.com>
2021-10-13 00:56:27 +0300

    register

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -17,6 +17,7 @@ const newUser = async (req, res) => {
                 errors: errors
             });
     }
+
     await UserModel.findOne({ userName: req.body.userName }, async (err, result) => {
         if (err) throw err;
         if (result) {
@@ -28,39 +29,48 @@ const newUser = async (req, res) => {
                     data: result
                 });
         }
-        const { firstName, lastName, email, phone, github, address, role, isApprove, userName } = req.body;
-        const newUser = new UserModel({
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            phone: phone,
-            role: role,
-            userName: userName,
-            github: github,
-            address: address,
-            isApprove: isApprove
-        });
-        try {
-            await newUser.save()
-            res
-                .status(201)
-                .json({
-                    success: true,
-                    message: "add new user",
-                    data: newUser
-                })
-        }
-        catch (err) {
-            res
-                .status(400)
-                .json({
-                    success: false,
-                    message: "failed crete user",
-                    error: err.message
-                })
-        }
+        SendEmails(req, res);
+        //Password Encryption Before That it enters to the database
+        bcrypt.genSalt(12, (err, salt) => {
+            if (err) throw err;
+            bcrypt.hash(req.body.password, salt, async (err, hash) => {
+                if (err) throw err;
+                req.body.password = hash;
 
-    })
+                const { firstName, lastName, email, phone, github, address, role, isApprove, userName } = req.body;
+                const newUser = new UserModel({
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    phone: phone,
+                    role: role,
+                    userName: userName,
+                    github: github,
+                    address: address,
+                    isApprove: isApprove
+                });
+                try {
+                    await newUser.save()
+                    res
+                        .status(201)
+                        .json({
+                            success: true,
+                            message: "add new user",
+                            data: newUser
+                        })
+                }
+                catch (err) {
+                    res
+                        .status(400)
+                        .json({
+                            success: false,
+                            message: "failed crete user",
+                            error: err.message
+                        })
+                }
+            });
+        });
+    });
 }
 
 const enterPassword = async (req, res) => {
@@ -70,15 +80,14 @@ const enterPassword = async (req, res) => {
     if (!Validator.isLength(req.body.password, { min: 8, max: 20 })) {
         errors.password = "Password must be at least 8 characters";
     }
-
     SendEmails(req, res);
     //Password Encryption Before That it enters to the database
     bcrypt.genSalt(12, (err, salt) => {
         if (err) throw err;
         bcrypt.hash(req.body.password, salt, async (err, hash) => {
             if (err) throw err;
-            req.body.password = hash;
             console.log(req.body.password)
+            req.body.password = hash;
             try {
                 const password = { $set: { password: req.body.password } };
                 await UserModel.findByIdAndUpdate(req.body.id,
