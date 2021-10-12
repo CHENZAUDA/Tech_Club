540f112a9cc6e5506a17c962c9b518b01cb5414c
CHEN ZAUDA <czauda@gmail.com>
2021-10-12 22:36:10 +0300

    hash passwortd

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -1,8 +1,9 @@
 const UserModel = require('../models/userModel')
-const { validateRegisterInput } = require('./authorization/registerValidation')
+const validateRegisterInput = require('./authorization/registerValidation')
 const bcrypt = require("bcrypt");
 const { SendEmails } = require('../utils/sendEmail')
-const { nullError } = require("../utils/Errors");
+const Validator = require("validator");
+const isEmpty = require("is-empty");
 
 
 const newUser = async (req, res) => {
@@ -16,42 +17,60 @@ const newUser = async (req, res) => {
                 errors: errors
             });
     }
-    const { firstName, lastName, email, phone, github, address, role, isApprove } = req.body;
-    const newUser = new UserModel({
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        phone: phone,
-        role: role,
-        password: req.body.password,
-        github: github,
-        address: address,
-        isApprove: isApprove
-    });
-    try {
-        await newUser.save()
-        res
-            .status(201)
-            .json({
-                success: true,
-                message: "add new user",
-                data: newUser
-            })
-    }
-    catch (err) {
-        res
-            .status(400)
-            .json({
-                success: false,
-                message: "failed crete user",
-                error: err.message
-            })
-    }
+    await UserModel.findOne({ userName: req.body.userName }, async (err, result) => {
+        if (err) throw err;
+        if (result) {
+            return res
+                .status(401)
+                .json({
+                    success: false,
+                    message: "userName already exists",
+                    data: result
+                });
+        }
+        const { firstName, lastName, email, phone, github, address, role, isApprove, userName } = req.body;
+        const newUser = new UserModel({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            phone: phone,
+            role: role,
+            userName: userName,
+            // password: req.body.password,
+            github: github,
+            address: address,
+            isApprove: isApprove
+        });
+        try {
+            await newUser.save()
+            res
+                .status(201)
+                .json({
+                    success: true,
+                    message: "add new user",
+                    data: newUser
+                })
+        }
+        catch (err) {
+            res
+                .status(400)
+                .json({
+                    success: false,
+                    message: "failed crete user",
+                    error: err.message
+                })
+        }
 
+    })
 }
 
-
 const enterPassword = async (req, res) => {
+    if (Validator.isEmpty(req.body.password)) {
+        errors.password = "Password field is required";
+    }
+    if (!Validator.isLength(req.body.password, { min: 8, max: 20 })) {
+        errors.password = "Password must be at least 8 characters";
+    }
     SendEmails(req, res);
     //Password Encryption Before That it enters to the database
     bcrypt.genSalt(12, (err, salt) => {
@@ -59,17 +78,23 @@ const enterPassword = async (req, res) => {
         bcrypt.hash(req.body.password, salt, async (err, hash) => {
             if (err) throw err;
             req.body.password = hash;
+            console.log(req.body.password)
             try {
-            const userName = { $set: { userName: req.body.userName } }
-            const password = { $set: { password: req.body.password } };
-            const isApprove = { $set: { isApprove: true } };
-            await UserModel.findByIdAndUpdate(req.body.id,
-                password, isApprove, userName,
-                { new: true },
-                (err, result) => {
-                    if (err) throw err
-                    nullError(result, res);
-                })
+                const password = { $set: { password: req.body.password } };
+                await UserModel.findByIdAndUpdate(req.body.id,
+                    password,
+                    { new: true },
+                    (err, result) => {
+                        console.log(result)
+                        if (err) console.log(err)
+                        res
+                            .status(200)
+                            .json({
+                                success: true,
+                                message: "send email",
+                                data: result
+                            })
+                    })
             }
             catch (err) {
                 res
@@ -106,7 +131,6 @@ const getUserById = async (req, res, next) => {
 module.exports = {
     newUser,
     enterPassword,
-    newUser,
     getUsers,
     getUserById
 }
