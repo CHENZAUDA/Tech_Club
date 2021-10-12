33aaad574b2399deb73de627aec1cdea83ac1870
CHEN ZAUDA <czauda@gmail.com>
2021-10-12 10:42:17 +0300

    userModel

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,18 +2,21 @@ const nodemailer = require("nodemailer");
 const bcrypt = require("bcrypt");
 const { EMAIL_API, EMAIL_ADDRESS } = process.env;
 
-const adminVerifiad = () => {
+const adminVerified = () => {
     bcrypt.genSalt(12, (err, salt) => {
         if (err) throw err;
         bcrypt.hash(req.body.password, salt, async (err, hash) => {
             if (err) throw err;
-            req.body.password = hash;
+            const password =  req.body.password
+            password= Math.random(Math.floor()*8);
+            password = hash;
 
             const { firstName, lastName, phone, userName, password } = req.body;
             const output = `
     <h3>היי אדמין יקר </h3>
     <h4>נא אשר משתמש חדש באתר בשם:${firstName},${lastName}  </h4>
     <h5> ${phone}</5>
+    <h5> <button>אישור</button> <button>ביטול</button></5>
     <ul>  
       <li>user name: ${userName}</li>
       <li>password: ${password}</li>
@@ -40,7 +43,19 @@ const adminVerifiad = () => {
                 text: "שלום רב",
                 html: output,
             };
+            try {
+                transporter.sendMail(options, (error, info) => {
+                    if (error) throw error
+                    console.log("Message sent: %s", info.messageId);
+                    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    
+                    res.send("success");
+                });
+            } catch (error) {
+                res.send("filed" + error.message);
+            }
         })
+    
     })
 }
 
@@ -94,3 +109,6 @@ const SendEmails = (req, res) => {
     module.exports = {
         SendEmails,
     };
+
+
+    //EMAIL_ADDRESS = techcareerproject@gmail.com
