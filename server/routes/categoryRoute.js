86e9a51e0a5f88173eef7f83df2ee83cee79287e
juda-star <juda.ishta@gmail.com>
2021-10-13 15:23:46 +0300

    comm

--- server/routes/categoryRoute.js
+++ server/routes/categoryRoute.js
@@ -4,7 +4,7 @@ const category = require('../controllers/categoryCtrl')
 
 router.post('/',category.newCategory)
 router.get('/',category.getAllCategory)
-router.get('/:id',category.getCategoryById)
+router.get('/getById',category.getCategoryById)
 router.put('/edit/:id',category.updateCategoryById)
 router.delete('/:id',category.deleteCategoryById)
 
