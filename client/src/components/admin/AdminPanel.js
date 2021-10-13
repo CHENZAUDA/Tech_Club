c5eba2adfd79448aa3413a64d74aafb4c965c7f7
YoniB1995 <yonatansamfisher@gmail.com>
2021-10-13 12:22:53 +0300

    updated yoni-branch

--- client/src/components/admin/AdminPanel.js
+++ client/src/components/admin/AdminPanel.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import AdminTablePanel from './AdminTablePanel'
 
 const AdminPanel = () => {
     return (
-        <div>
+        <div className="admin-panel-body">
             <h1>Admin Panel</h1>
+            <h4>Users Data</h4>
+            <AdminTablePanel/>
         </div>
     )
 }
